2b9150ae4447940866898ebc7b28ca25ad0a9d42
Destin McMurry <destinmcmurry@gmail.com>
2018-08-25 17:13:48 -0400

    updated style again

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ import './Navbar.css';
 const Navbar = props => {
   return (
     <div className='Navbar'>
-      <Link id='small-width' to='/blurb'><span>blurb.</span></Link>
-      <Link to='/timeline'><span>timeline. </span></Link>
-      <Link to='/projects'><span> projects.</span></Link>
-      <Link to='/misc'><span>misc.</span></Link>
+      <Link id='small-width' to='/blurb'><span>blurb</span></Link>
+      <Link to='/timeline'><span>timeline </span></Link>
+      <Link to='/projects'><span> projects</span></Link>
+      <Link to='/misc'><span>misc</span></Link>
     </div>
   );
 }
