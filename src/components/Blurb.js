0450278bb7e23bc6b9f1729f15f47fbccf975ccd
Destin McMurry <destinmcmurry@gmail.com>
2018-08-26 17:19:41 -0400

    style updates

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -13,12 +13,14 @@ const headerStyle = {
   fontSize: '16px',
   margin: '5px auto 0 auto',
   paddingBottom: '5px',
-  width: '150px',
+  width: '30%',
+  minWidth: '150px',
   borderBottom: '1px dotted #3d3d3d'
 }
 
-const valuesStyle = {
-  marginTop: '10px'
+const lineStyle = {
+  margin: '5px',
+  verticalAlign: 'middle'
 }
 
 const Blurb = props => {
@@ -26,33 +28,33 @@ const Blurb = props => {
     <div className='Blurb'>
       <img style={imageStyle} src='https://avatars3.githubusercontent.com/u/29695784?s=460&v=4' alt='headshot'/>
       <h4 style={headerStyle}>Myself</h4>
-      <p>Before coding, I spent most of my time in art and architecture studios, where I first began thinking about flow and function, realized I would need to teach myself the tools and technologies I needed to succeed and stand out, and learned to see the big picture and at the same time know that the devil is in the details. As an engineer, I bring these lessons with me as well as something that most creatives share: the burning desire to be proud of what I make.</p>
+      <p>Before coding, I spent most of my time in art and architecture studios, where I first began thinking about flow and function, realized I would need to teach myself the tools and technologies I needed to stand out, and learned to see the big picture while also knowing that the devil is in the details. As an engineer, I bring these lessons with me as well as something that most creatives share: the burning desire to be proud of what I make.</p>
       <h4 style={headerStyle}>My Goals</h4>
       <p>I'm looking to be part of a collaborative environment where I have the opportunity to tackle challenges, contribute ideas, and learn from others on a daily basis.</p>
       <h4 style={headerStyle}>My Values</h4>
-      <div style={valuesStyle}>
+      <div style={{marginTop: '10px'}}>
         <span>open communication</span>
-        <span> | </span>
+        <span style={lineStyle}> | </span>
         <span>sharing</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>organization and best practices</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>celebrating wins</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>a constructive mindset</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>a general sense of awareness and consideration</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>efficiency</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>not taking yourself too seriously</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>thoughtful feedback</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>staying iterative</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>positivity</span>
-        <span> | </span>
+         <span style={lineStyle}> | </span>
         <span>the little things</span>
       </div>
       <br />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ import './Navbar.css';
 const Navbar = props => {
   return (
     <div className='Navbar'>
-      <Link id='small-width' to='/blurb'><span>blurb</span></Link>
+      <Link id='about' to='/blurb'><span>about</span></Link>
       <span className='seperator'>|</span>
-      <Link to='/timeline'><span>timeline </span></Link>
+      <Link to='/timeline'><span>timeline</span></Link>
       <span className='seperator'>|</span>
-      <Link to='/projects'><span> projects </span></Link>
+      <Link id ='projects' to='/projects'><span> projects</span></Link>
       <span className='seperator'>|</span>
       <Link id='media' to='/media'><span>media</span></Link>
     </div>
