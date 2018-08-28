652423cffaf262986911518084d29daa44d2ab47
Destin McMurry <destinmcmurry@gmail.com>
2018-08-28 13:36:52 -0400

    final image fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -40,7 +40,7 @@ const Projects = props => {
 
       <a href='https://github.com/acdc-1802/pintrips' target='blank'><img style={logoStyle} src='https://image.flaticon.com/icons/svg/25/25231.svg' alt='github'/></a>
       <h4 style={headerStyle}>Pintrips</h4>
-      <a href='https://mypintrips.com/' target='blank'><img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/pintrips-screenshot.png' alt='pintrips'/></a>
+      <a href='https://mypintrips.com/' target='blank'><img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/pintrips-screenshot.png?raw=true' alt='pintrips'/></a>
       <p style={dottedLineStyle}></p>
       <h4>a progressive web application for collaboratively planning and journaling trips</h4>
       <small>Pintrips allows users to plan for and journal trips by dropping custom pins to mark places they would like to go and places that they've been on individual 'place boards'. These boards, rendered with a React binding of Mapbox GL JS, are shareable and can have multiple collaborators and styles. The real-time collaboration is possible because of the use of Google's Cloud Firestore, which allows board components to listen for any changes and automatically update when they happen. At any point in the trip, users can send their pinned and styled boards as an online postcard that uses app geolocation and GreenSock animation to stamp and send to family and friends.</small>
@@ -51,7 +51,7 @@ const Projects = props => {
 
       <a href='https://github.com/deb-gh-1802/grace-shopper' target='blank'><img style={logoStyle} src='https://image.flaticon.com/icons/svg/25/25231.svg' alt='github'/></a>
       <h4 style={headerStyle}>Build - A - Self</h4>
-      <a href='https://rocky-eyrie-74701.herokuapp.com/' target='blank'><img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/buildaself-screenshot.png' alt='build-a-self'/></a>
+      <a href='https://rocky-eyrie-74701.herokuapp.com/' target='blank'><img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/buildaself-screenshot.png?raw=true' alt='build-a-self'/></a>
       <p style={dottedLineStyle}></p>
       <h4>an e-commerce website that sells desirable qualities</h4>
       <small>Build-A-Self is an e-commerce site that sells confidence, ambition, and other positive qualities. The functionality includes creating an account with an email or Google OAuth, adding items to your cart, purchasing an order (well, sort of), and viewing your past order history. For administrators, there is also a content management system for adding and updating items, altering admin statuses, and viewing all users' order histories.</small>
@@ -61,7 +61,7 @@ const Projects = props => {
 
       <a href='https://github.com/destinmcmurry/happy-router-web' target='blank'><img style={logoStyle} src='https://image.flaticon.com/icons/svg/25/25231.svg' alt='github'/></a>
       <h4 style={headerStyle}>Happy Router</h4>
-      <img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/happyrouter-screenshot.png' alt='happyrouter'/>
+      <img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/happyrouter-screenshot.png?raw=true' alt='happyrouter'/>
       <h4>a mobile responsive web app that generates optimal routes of nearby happy hours</h4>
       <small>HappyRouter generates a randomized route of restaurant and bar specials based on a user's input. It does all of the research and planning for you by searching for active happy hours that are nearby and fall within your window, and ordering the route with a maximum of 3 places by the soonest ending happy hour.</small>
       <p>stack: JavaScript, HTML, CSS, Node.js, React.js, Redux, React-Redux, Mapbox GL JS</p>
@@ -71,7 +71,7 @@ const Projects = props => {
 
       <a href='https://github.com/destinmcmurry/wagstaff/' target='blank'><img style={logoStyle} src='https://image.flaticon.com/icons/svg/25/25231.svg' alt='github'/></a>
       <h4 style={headerStyle}>Wagstaff</h4>
-      <img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/wagstaff-screenshot.png' alt='happyrouter'/>
+      <img style={screenshotStyle}  src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/wagstaff-screenshot.png?raw=true' alt='happyrouter'/>
       <h4>a single page application for managing a database of students and homerooms</h4>
       <small>The Wagstaff Elementary School Management site organizes students into their corresponding homerooms and allows the user to add new homerooms and students easily by filling out a quick form and to delete homerooms and students with the click of a button. If a teacher's homeroom is deleted, all students previously assigned will be updated to display "no homeroom."</small>
       <p>stack: JavaScript, HTML, CSS, Node.js, Express, React.js, Redux, React-Redux, PostgreSQL, Sequelize</p>
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -42,7 +42,7 @@ const Timeline = props => {
   return (
     <div style={tlStyle}>
     <span style={yearStyle}>2010</span>
-    <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/msBanner.jpg' alt='rural ms'/>
+    <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/msBanner.jpg?raw=true' alt='rural ms'/>
       <span style={segmentStyle}>accepted to the Mississippi School for Math and Science in Columbus, MS</span>
       <span style={segmentStyle}>:</span>
       <span style={yearStyle}>2011</span>
@@ -51,7 +51,7 @@ const Timeline = props => {
       <span style={yearStyle}>2012</span>
       <span style={segmentStyle}>first exploration of architecture with an internship at Mississippi State University where I first tried my hand at PhotoShop and gathered research for a Rural Downtown Development thesis</span>
       <span style={segmentStyle}>:</span>
-      <img style={bannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/msmsBanner.jpg' alt='senior picture'/>
+      <img style={bannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/msmsBanner.jpg?raw=true' alt='senior picture'/>
       <span style={segmentStyle}>graduated from MSMS and started a countdown to my first flight</span>
       <span style={segmentStyle}>:</span>
       <span style={segmentStyle}>began freshman year at Vassar College as a National Questbridge Scholar</span>
@@ -59,7 +59,7 @@ const Timeline = props => {
       <span style={segmentStyle}>getting the most out of my liberal arts experience by exploring nearly every department and becoming increasingly unsure of my future</span>
       <span style={segmentStyle}>:</span>
       <span style={yearStyle}>2013</span>
-      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/vcBanner.jpg' alt='vassar library'/>
+      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/vcBanner.jpg?raw=true' alt='vassar library'/>
       <span style={segmentStyle}>began working as an Art Lab Assistant for Vassar's Professor of Gothic Architecture and practicing my color correcting and 360º panorama stitching</span>
       <span style={segmentStyle}>:</span>
       <span style={segmentStyle}>first architectural design studio and introduction to tools like AutoCAD, Illustrator, and SketchUp</span>
@@ -67,7 +67,7 @@ const Timeline = props => {
       <span style={yearStyle}>2014</span>
       <span style={segmentStyle}>chosen as a Vassar Ford Scholar for the project "Transcribing the Gothic Cathedral" where I worked with laser data and really honed my AutoCAD skills</span>
       <span style={segmentStyle}>:</span>
-      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/denmarkBanner.jpg' alt='denmark'/>
+      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/denmarkBanner.jpg?raw=true' alt='denmark'/>
       <span style={segmentStyle}>enrolled in the Foundations Architecture program at the Danish Institute for Study Abroad in Copenhagen, Denmark where I regularly traveled for site studies, took a watercolor class, and completed my favorite architectural design project "Planting a Community Food Hub"</span>
       <span style={segmentStyle}>:</span>
       <span style={yearStyle}>2015</span>
@@ -76,14 +76,14 @@ const Timeline = props => {
       <span style={segmentStyle}>designed and built a 15'x9'x6' sculpture entirely of metal rod that challenged me come up with an assembly plan that would have the least amount of welds possible and take the least amount of material and time</span>
       <span style={segmentStyle}>:</span>
       <span style={yearStyle}>2016</span>
-      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/libraryBanner.jpg' alt='vassar library'/>
+      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/libraryBanner.jpg?raw=true' alt='vassar library'/>
       <span style={segmentStyle}>completed 50-page senior thesis on creative placemaking in the local community organization scene and field work with a low-income housing organization in Poughkeepsie, NY</span>
       <span style={segmentStyle}>:</span>
       <span style={segmentStyle}>graduated from Vassar with a degree in Urban Studies with a concentration in architectural design</span>
       <span style={segmentStyle}>:</span>
       <span style={segmentStyle}>decided to move to Seoul, SK to begin teaching English at an adult language academy and avoid making the grad school decision</span>
       <span style={segmentStyle}>:</span>
-      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/koreaBanner.jpg' alt='seoul, sk'/>
+      <img style={lighterBannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/koreaBanner.jpg?raw=true' alt='seoul, sk'/>
       <span style={yearStyle}>2017</span>
       <span style={segmentStyle}>began teaching myself HTML, CSS, and JavaScript because I thought they might be cool skills to have</span>
       <span style={segmentStyle}>:</span>
@@ -91,7 +91,7 @@ const Timeline = props => {
       <span style={segmentStyle}>:</span>
       <span style={yearStyle}>2018</span>
       <span style={segmentStyle}>moved to Brooklyn, NY to begin my Full-Stack JavaScript Developer training</span>
-      <img style={bannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/bridgeBanner.jpg' alt='brooklyn bridge'/>
+      <img style={bannerStyle} src='https://github.com/destinmcmurry/personal-website/blob/master/public/images/bridgeBanner.jpg?raw=true' alt='brooklyn bridge'/>
       <span style={segmentStyle}>:</span>
       <span style={segmentStyle}>presented my final project on facebook live without passing out and graduated from Grace Hopper eager to find my next role as a front-end or full-stack software developer!</span>
     </div>
